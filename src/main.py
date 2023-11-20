2d8a99202c629d491a491ad88154ca9599f8d372
regisgambiza <regisgambiza@gmail.com>
2023-11-21 00:44:28 +0200

    Initial setup for day one

--- src/main.py
+++ src/main.py
@@ -1,1 +1,21 @@
+# main.py
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from src.app.school_manager import Ui_MainWindow
 
+
+class SchoolManagerApp(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)  # This sets up the UI from the generated file
+
+
+def main():
+    app = QApplication(sys.argv)
+    school_manager_app = SchoolManagerApp()
+    school_manager_app.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
